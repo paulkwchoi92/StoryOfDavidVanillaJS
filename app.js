3d5013c8186ab338afd4b9a2347f7f5cc13cd586
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-15 13:07:55 -0700

    database conneciton for users

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://storyofdavidadmin:2J5R4syhsooL9rwe@cluster0-ach4e.mongodb.net/test?retryWrites=true&w=majority";
+
 var express = require("express");
 var app = express();
 var serv = require("http").Server(app);
@@ -5,6 +9,11 @@ var serv = require("http").Server(app);
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/index.html");
 });
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
 app.use("/client", express.static(__dirname + "/client"));
 
 serv.listen(2000);
@@ -27,6 +36,10 @@ var Entity = function () {
     self.x += self.spdX;
     self.y += self.spdY;
   };
+
+  self.getDistance = function (pt) {
+    return Math.sqrt(Math.pow(self.x - pt.x, 2) + Math.pow(self.y - pt.y, 2));
+  };
   return self;
 };
 
@@ -38,12 +51,29 @@ var Player = function (id) {
   self.pressingLeft = false;
   self.pressingUp = false;
   self.pressingDown = false;
+  self.pressingAttack = false;
+  self.attackMultiple = false;
+  self.mouseAngle = 0;
   self.maxSpd = 10;
 
   var super_update = self.update;
   self.update = function () {
     self.updateSpd();
     super_update();
+
+    if (self.pressingAttack) {
+      if (self.attackMultiple) {
+        for (var i = -3; i < 3; i++) self.shootBullet(self.mouseAngle);
+      } else {
+        self.shootBullet(self.mouseAngle);
+      }
+    }
+  };
+
+  self.shootBullet = function (angle) {
+    var b = Bullet(self.id, angle);
+    b.x = self.x;
+    b.y = self.y;
   };
 
   self.updateSpd = function () {
@@ -66,6 +96,12 @@ Player.onConnect = function (socket) {
     else if (data.inputId === "right") player.pressingRight = data.state;
     else if (data.inputId === "up") player.pressingUp = data.state;
     else if (data.inputId === "down") player.pressingDown = data.state;
+    else if (data.inputId === "attack") player.pressingAttack = data.state;
+    else if (data.inputId === "mouseAngle") player.mouseAngle = data.state;
+    else if (data.inputId === "attackMultiple") {
+      player.attackMultiple = data.state;
+      player.pressingAttack = data.state;
+    }
   });
 };
 Player.onDisconnect = function (socket) {
@@ -85,18 +121,26 @@ Player.update = function () {
   return pack;
 };
 
-var Bullet = function (angle) {
+var Bullet = function (parent, angle) {
   var self = Entity();
   self.id = Math.random();
   self.spdX = Math.cos((angle / 180) * Math.PI) * 10;
   self.spdY = Math.sin((angle / 180) * Math.PI) * 10;
-
+  self.parent = parent;
   self.timer = 0;
   self.toRemove = false;
   var super_update = self.update;
   self.update = function () {
     if (self.timer++ > 100) self.toRemove = true;
     super_update();
+
+    for (var i in Player.list) {
+      var p = Player.list[i];
+      if (self.getDistance(p) < 32 && self.parent !== p.id) {
+        // handle collsion. ex: hp --
+        self.toRemove = true;
+      }
+    }
   };
   Bullet.list[self.id] = self;
   return self;
@@ -104,14 +148,15 @@ var Bullet = function (angle) {
 Bullet.list = {};
 
 Bullet.update = function () {
-  if (Math.random() < 0.1) {
-    Bullet(Math.random() * 360);
-  }
+  // if (Math.random() < 0.1) {
+  //   Bullet(Math.random() * 360);
+  // }
 
   var pack = [];
   for (var i in Bullet.list) {
     var bullet = Bullet.list[i];
     bullet.update();
+    if (bullet.toRemove) delete Bullet.list[i];
     pack.push({
       x: bullet.x,
       y: bullet.y,
@@ -120,16 +165,81 @@ Bullet.update = function () {
   return pack;
 };
 
+var DEBUG = true;
+var USERS = {
+  //username:password
+  bob: "asd",
+  bob2: "bob",
+  bob3: "ttt",
+};
+
+var isValidPassword = function (data, cb) {
+  db.account.find(
+    { username: data.username, password: data.password },
+    function (err, res) {
+      if (res.length > 0) cb(true);
+      else cb(false);
+    }
+  );
+};
+var isUsernameTaken = function (data, cb) {
+  db.account.find({ username: data.username }, function (err, res) {
+    if (res.length > 0) cb(true);
+    else cb(false);
+  });
+};
+var addUser = function (data, cb) {
+  db.account.insert(
+    { username: data.username, password: data.password },
+    function (err) {
+      cb();
+    }
+  );
+};
+
 var io = require("socket.io")(serv, {});
 io.sockets.on("connection", function (socket) {
   socket.id = Math.random();
   SOCKET_LIST[socket.id] = socket;
 
-  Player.onConnect(socket);
+  socket.on("signIn", function (data) {
+    isValidPassword(data, function (res) {
+      if (res) {
+        Player.onConnect(socket);
+        socket.emit("signInResponse", { success: true });
+      } else {
+        socket.emit("signInResponse", { success: false });
+      }
+    });
+  });
+
+  socket.on("signUp", function (data) {
+    isUsernameTaken(data, function (res) {
+      if (res) {
+        socket.emit("signUpResponse", { success: false });
+      } else {
+        addUser(data, function () {
+          socket.emit("signUpResponse", { success: true });
+        });
+      }
+    });
+  });
 
   socket.on("disconnect", function () {
     delete SOCKET_LIST[socket.id];
     Player.onDisconnect(socket);
+  });
+  socket.on("sendMsgToServer", function (data) {
+    var playerName = ("" + socket.id).slice(2, 7);
+    for (var i in SOCKET_LIST) {
+      SOCKET_LIST[i].emit("addToChat", playerName + ": " + data);
+    }
+  });
+  //remove under code before deloying
+  socket.on("evalServer", function (data) {
+    if (!DEBUG) return;
+    var res = eval(data);
+    socket.emit("evalAnswer", res);
   });
 });
 
---
+++ models/account.js
@@ -0,0 +1,23 @@
+// At the top of the file, import Mongoose. We will also need to require the Mongoose Schema:
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Now, let's think ahead to the information we need to require from a user and setup our schema:
+const AccountSchema = new Schema({
+  // profile pictures
+  username: {
+    type: String,
+    required: false 
+  },
+  password: {
+    type: String,
+    required:false
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Let's not forget to export our model.
+module.exports = User = mongoose.model("account", AccountSchema);
