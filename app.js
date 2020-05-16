89668b9b59f187117c76e20224694add49619e0d
paulkwchoi92 <paulkwchoi92@gmail.com>
2020-05-15 17:50:00 -0700

    changing servercode

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ require("./client/Inventory");
 
 var express = require("express");
 var app = express();
-var serv = require("http").Server(app);
+// var serv = require("http").Server(app);
+var serv = app.listen(process.env.PORT || 2000)
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/client/index.html");
 });
 app.use("/client", express.static(__dirname + "/client"));
 
-serv.listen(process.env.PORT || 2000);
+// serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
